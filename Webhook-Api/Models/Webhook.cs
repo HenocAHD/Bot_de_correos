0c3e006732a62d6a10cd2e53df22d32ef2cf2ca8
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2022-09-27 18:14:25 -0600

    Modelo webhook arreglado

--- Webhook-Api/Models/Webhook.cs
+++ Webhook-Api/Models/Webhook.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using Linkedin.Net.Db;
 using Linkedin.Net.Models;
 using Linkedin.Net.Models.Interfaces;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace Webhook_Api.Models
 {
-    public class webhook:IModel
+    public class webhook : IModel
     {
         private DateTime _create_at;
 
         public int id_webhook_data { get; set; }
         public string email { get; set; }
-        public string @event { get; set; }
+        [JsonProperty("@event")]
+        [JsonPropertyName("@event")]
+        public string evento { get; set; }
         public string ip { get; set; }
         public string sg_content_type { get; set; }
         public string sg_event_id { get; set; }
@@ -44,11 +48,11 @@ namespace Webhook_Api.Models
                 var sql = "";
                 if (id_webhook_data != 0)
                 {
-                    sql = $"INSERT INTO webhook_data (id_webhook_data, email, event, ip, sg_content_type, sg_event_id, sg_machine_open, sg_message_id, timestamp, useragent, create_at) VALUES (@id_webhook_data, @email, @event, @ip, @sg_content_type, @sg_event_id, @sg_machine_open, @sg_message_id, @timestamp, @useragent, DATE_FORMAT(@create_at,'%Y-%m-%d %H:%i:%s')); ";
+                    sql = $"INSERT INTO webhook_data (id_webhook_data, email, evento, ip, sg_content_type, sg_event_id, sg_machine_open, sg_message_id, timestamp, useragent, create_at) VALUES (@id_webhook_data, @email, @evento, @ip, @sg_content_type, @sg_event_id, @sg_machine_open, @sg_message_id, @timestamp, @useragent, DATE_FORMAT(@create_at,'%Y-%m-%d %H:%i:%s')); ";
                 }
                 else
                 {
-                    sql = $"INSERT INTO webhook_data (id_webhook_data, email, event, ip, sg_content_type, sg_event_id, sg_machine_open, sg_message_id, timestamp, useragent, create_at) VALUES (DEFAULT, @email, @event, @ip, @sg_content_type, @sg_event_id, @sg_machine_open, @sg_message_id, @timestamp, @useragent, DATE_FORMAT(@create_at,'%Y-%m-%d %H:%i:%s')); ";
+                    sql = $"INSERT INTO webhook_data (id_webhook_data, email, evento, ip, sg_content_type, sg_event_id, sg_machine_open, sg_message_id, timestamp, useragent, create_at) VALUES (DEFAULT, @email, @evento, @ip, @sg_content_type, @sg_event_id, @sg_machine_open, @sg_message_id, @timestamp, @useragent, DATE_FORMAT(@create_at,'%Y-%m-%d %H:%i:%s')); ";
                 }
                 var result = db.Execute(sql, this);
                 return (result > 0);
@@ -68,7 +72,7 @@ namespace Webhook_Api.Models
         {
             using (var db = database.connection)
             {
-                var sql = "UPDATE webhook_data SET email = @email, event = @event, ip = @ip, sg_content_type = @sg_content_type, sg_event_id = @sg_event_id, sg_machine_open = @sg_machine_open, sg_message_id = @sg_message_id, timestamp = @timestamp, useragent = @useragent, required = @required WHERE id_webhook_data = @id_webhook_data;";
+                var sql = "UPDATE webhook_data SET email = @email, evento = @evento, ip = @ip, sg_content_type = @sg_content_type, sg_event_id = @sg_event_id, sg_machine_open = @sg_machine_open, sg_message_id = @sg_message_id, timestamp = @timestamp, useragent = @useragent, required = @required WHERE id_webhook_data = @id_webhook_data;";
                 var result = db.Execute(sql, this);
                 return (result > 0);
             }
@@ -82,7 +86,7 @@ namespace Webhook_Api.Models
                 var result = db.Query<webhook>(sql);
                 return (result.ToList());
             }
-        } 
+        }
 
         public static webhook SelectById(database database, string id_webhook_data)
         {
